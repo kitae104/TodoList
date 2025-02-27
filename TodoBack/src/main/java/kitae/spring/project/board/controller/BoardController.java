09edb583ed8221416c115cfe207a3ce1b9b3f935
Kitae's Github <aqua0405@gmail.com>
2025-02-28 00:18:54 +0900

    CRUD 작업

--- TodoBack/src/main/java/kitae/spring/project/board/controller/BoardController.java
+++ TodoBack/src/main/java/kitae/spring/project/board/controller/BoardController.java
@@ -18,6 +18,7 @@ public class BoardController {
 
   private final BoardService boardService;
 
+  // 게시판 리스트 조회
   @GetMapping("")
   public ResponseEntity<?> getTodosList(
           @RequestParam(defaultValue = "0") int page, // 현재 페이지
@@ -26,6 +27,76 @@ public class BoardController {
     try {
       Page<BoardDto> boardDtoList = boardService.getBoardList(page, size);
       return new ResponseEntity<>(boardDtoList, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  // 게시판 상세 조회
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getTodoById(@PathVariable Long id) {
+    try {
+      BoardDto boardDto = boardService.getBoardById(id);
+      return new ResponseEntity<>(boardDto, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  // 게시판 추가
+  @PostMapping()
+  public ResponseEntity<?> addTodo(@RequestBody BoardDto boardDto) {
+    try {
+      boolean result = boardService.addBoard(boardDto);
+      if (result) {
+        return new ResponseEntity<>("SUCCESS", HttpStatus.CREATED);
+      } else {
+        return new ResponseEntity<>("FAIL", HttpStatus.BAD_REQUEST);
+      }
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> updateTodo(@RequestBody BoardDto boardDto) {
+    log.info("BoardDto = " + boardDto);
+    try {
+      Long id = boardDto.getId();
+      boolean result = false;
+
+      if(id == null) {
+        throw new IllegalArgumentException("id is null");
+      } else {
+        log.info("updateTodoById");
+        result = boardService.updateBoardById(boardDto);
+      }
+      if (result) {
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+      } else {
+        return new ResponseEntity<>("FAIL", HttpStatus.BAD_REQUEST);
+      }
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @DeleteMapping({"/{id}", ""})
+  public ResponseEntity<?> deleteTodoById(@PathVariable(value="id", required = false) Long id) {
+    try {
+      boolean result = false;
+      if(id == null) {
+        log.info("deleteAll");
+        result = boardService.deleteAll();
+      } else {
+        log.info("deleteTodoById");
+        result = boardService.deleteBoardById(id);
+      }
+      if (!result) {
+        return new ResponseEntity<>("FAIL", HttpStatus.BAD_REQUEST);
+      } else {
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+      }
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- TodoBack/src/main/java/kitae/spring/project/board/service/BoardService.java
+++ TodoBack/src/main/java/kitae/spring/project/board/service/BoardService.java
@@ -7,10 +7,7 @@ import kitae.spring.project.todo.dto.TodoDto;
 import kitae.spring.project.todo.entity.Todo;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -25,14 +22,14 @@ public class BoardService {
   private final ModelMapper modelMapper;
 
   public Page<BoardDto> getBoardList(int page, int size) {
-    Pageable pageable = PageRequest.of(page, size); // pageable 객체 생성
+    Pageable pageable = PageRequest.of(page, size, Sort.by("regTime").descending()); // pageable 객체 생성
     Page<Board> boardListPage = boardRepository.findAll(pageable);
     Page<BoardDto> boardDtoListPage = boardListPage.map(board -> modelMapper.map(board, BoardDto.class));
 
     // status, seq 순으로 정렬
     List<BoardDto> sortedList = boardDtoListPage.getContent().stream()
-            .sorted(Comparator.comparing(BoardDto::getId))  // status 기준 오름차순
-            //.thenComparing(BoardDto::getSeq)) // seq 기준 오름차순
+//            .sorted(Comparator.comparing(BoardDto::getId))  // status 기준 오름차순
+//            .thenComparing(BoardDto::getSeq)) // seq 기준 오름차순
             .collect(Collectors.toList());
 
     // 새로운 Page 객체 반환 (페이지 정보 유지)
@@ -47,5 +44,34 @@ public class BoardService {
     } else {
       return true;
     }
+  }
+
+  public BoardDto getBoardById(Long id) {
+    Board board = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+    BoardDto boardDto = modelMapper.map(board, BoardDto.class);
+    return boardDto;
+  }
+
+  public boolean updateBoardById(BoardDto boardDto) {
+    Board board = boardRepository.findById(boardDto.getId()).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+    board.setTitle(boardDto.getTitle());
+    board.setWriter(boardDto.getWriter());
+    board.setContent(boardDto.getContent());
+    Board updatedBoard = boardRepository.save(board);
+    if(updatedBoard == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public boolean deleteAll() {
+    boardRepository.deleteAll();
+    return true;
+  }
+
+  public boolean deleteBoardById(Long id) {
+    boardRepository.deleteById(id);
+    return true;
   }
 }
